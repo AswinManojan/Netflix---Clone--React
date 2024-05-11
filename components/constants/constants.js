5055bcfe92f4b62e276706bace6dbae9dec45635
Aswin M <125151196+AswinManojan@users.noreply.github.com>
2024-05-11 13:33:48 +0530

    Update constants.js

--- components/constants/constants.js
+++ components/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl ="https://api.themoviedb.org/3";
-export const API_KEY="ef035ebacef16d27381a59255eebcdeb";
+export const API_KEY="-----------ENTER API KEY------------------";
 export const imageURL = "https://image.tmdb.org/t/p/original";
